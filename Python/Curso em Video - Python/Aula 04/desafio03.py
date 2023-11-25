ba0cd7a3748241af57276cf00f6a1fd5efc9f538
Geovana Matos <geovanamatos@gmail.com>
2023-11-25 12:24:26 -0300

    Apagando comentários do desafio03.py

--- Python/Curso em Video - Python/Aula 04/desafio03.py
+++ Python/Curso em Video - Python/Aula 04/desafio03.py
@@ -1,9 +1,9 @@
 #Crie um script Python que leia dois números e tente 
 #mostrar a soma entre eles.
 
-numero1 = int(input('Digite o primeiro número: ')) #10 / 30
-numero2 = int(input('Digite o segundo número: ')) #10 / 20
+numero1 = int(input('Digite o primeiro número: ')) 
+numero2 = int(input('Digite o segundo número: ')) 
 
-soma = numero1 + numero2 #20 / 50
+soma = numero1 + numero2 
 
-print(f'A soma entre os dois números digitados é: {soma}') #20 / 50
+print(f'A soma entre os dois números digitados é: {soma}') 
